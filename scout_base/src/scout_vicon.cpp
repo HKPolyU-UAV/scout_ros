715fac18172cbc79a73bfb5cb7a2a995ccfaee43
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2021-09-13 21:16:13 +0800

    Update scout_vicon.cpp

--- scout_base/src/scout_vicon.cpp
+++ scout_base/src/scout_vicon.cpp
@@ -48,11 +48,12 @@ Vec2 Poistion_controller_PID(Vec3 pose_XYyaw, Vec2 setpoint){ // From VRPN XY po
     // cout << "err_dist: " << err_dist << endl;
     Vec2 diff_XY = Vec2(setpoint[0]-pose_XYyaw[0],setpoint[1]-pose_XYyaw[1]);
     double err_yaw = atan2(diff_XY[1],diff_XY[0])-pose_XYyaw[2];
-    if (err_yaw>=PI){  err_yaw-=2*PI;}
-    if (err_yaw<=-PI){ err_yaw+=2*PI;}
     // cout << "err_yaw: " << err_yaw << endl;
-    if (err_dist<0.1){err_dist = 0;err_yaw = 0;} // Stop if the error is within 10 cm
-    if (err_yaw>PI*0.1||err_yaw<PI*-0.1){ err_dist = 0; } //Turn before going straight
+    if (err_dist<0.2 && err_yaw > PI*0.3){err_dist = 0;err_yaw = 0;}
+
+    if (err_dist<0.1){err_dist = 0;err_yaw = 0;}            // Stop if the error is within 10 cm
+    
+    if (err_yaw>PI*0.3||err_yaw<PI*-0.3){ err_dist = 0; }   //Turn before going straight
 
     Vec2 error,last_error,u_p,u_i,u_d,output; // Dist Yaw Error
     double Last_time = ros::Time::now().toSec();
@@ -64,17 +65,17 @@ Vec2 Poistion_controller_PID(Vec3 pose_XYyaw, Vec2 setpoint){ // From VRPN XY po
     last_error = error;
     Vec2 integral = integral+(error*iteration_time);
     Vec2 derivative = (error - last_error)/iteration_time;
-    for (int i=0; i<2; i++){             //i = straight_distance,Yaw
-        u_p[i] = error[i]*K_p[i];        //P controller
-        u_i[i] = integral[i]*K_i[i];     //I controller
-        u_d[i] = derivative[i]*K_d[i];   //D controller
+    for (int i=0; i<2; i++){                //i = straight_distance,Yaw
+        u_p[i] = error[i]*K_p[i];           //P controller
+        u_i[i] = integral[i]*K_i[i];        //I controller
+        u_d[i] = derivative[i]*K_d[i];      //D controller
         output[i] = u_p[i]+u_i[i]+u_d[i];
     }
-    cout << "output_raw v: " << output[0] << " av: " << output[1] << endl;
-      if(output[0] >  0.8){ output[0]= 0.8;} //Clamp the forward speed to 1.5 m/s
+    
+    if(output[0] >  0.8){ output[0]= 0.8;}  //Clamp the forward speed to 0.8 m/s
 
-      if(output[1] >  1.5){ output[1]= 1.5;}
-      if(output[1] < -1.5){ output[1]= 1.5;}
+    if(output[1] >  1){ output[1] = 1;}
+    if(output[1] < -1){ output[1] = 1;}
 
     cout << "output____ v: " << output[0] << " av: " << output[1] << endl;
     return(output);
@@ -84,16 +85,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "scout_vicon");
     ros::NodeHandle nh;
     // ros::Subscriber sub =nh.subscribe("/cmd_vel_raw",50,callback);
-    ros::Subscriber ugvpose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/vrpn_client_node/gh034_car/pose", 1, UGVPose_cb);
-    ros::Subscriber ugvdespose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/scout_wp/pose", 1, UGVdesPose_cb);
+    ros::Subscriber ugvpose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/vrpn_client_node/gh034_car/pose", 5, UGVPose_cb);
+    ros::Subscriber ugvdespose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/scout_wp/pose", 5, UGVdesPose_cb);
     ros::Publisher pub_twist =nh.advertise<geometry_msgs::Twist>("/cmd_vel",5);
     ros::Rate ros_rate(10);
     cout << "Super Car Initialized" << endl;
 
     while(ros::ok())
     {
-      // Vec2 XY = Vec2(-5,0.8);
-      Vec2 XY = DesUGVpose;
+      Vec2 XY = Vec2(5,-5);
+      Vec3 pose_XYyaw = Vec3(0,0,0);
+    //   Vec2 XY = DesUGVpose;
 
       twist_pub(Poistion_controller_PID(pose_XYyaw,XY));
       pub_twist.publish(UGV_twist_pub);

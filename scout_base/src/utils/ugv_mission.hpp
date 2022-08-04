18adc23dec4222ba1ee82739fd602497f2e7d7e4
pattylo <19451945as@gmail.com>
2022-08-04 21:33:43 +0800

    scout

--- scout_base/src/utils/ugv_mission.hpp
+++ scout_base/src/utils/ugv_mission.hpp
@@ -9,10 +9,13 @@ deque<Vec4> Finite_stage_mission(){ //Normal mission
     waypoints.clear();
     // Waypoints
     Vec4 stage; // state x y duration
+
     stage << 1, -1.2, -1.4, 120; //stage, x, y, second
     waypoints.push_back(stage);
+
     stage << 1, -1.2, -1.4, 120;
     waypoints.push_back(stage);
+    
     cout << " Mission generated!" << " Stage count: " << waypoints.size() << endl;
     return(waypoints);
 }
